4120f62dfeae3506f3dbfba11c2388b9ec82bebc
klepy <klepy9gle@gmail.com>
2021-06-30 18:28:23 +0900

    Committed 06-30

--- README.md
+++ README.md
@@ -3,3 +3,34 @@ Implementation of the vanilla fast federated learning paper : [Fast Federated Le
 
 Experiments are produced on MNIST and CIFAR10 (both IID and non-IID). In case of non-IID, the data amongst the users can be split equally or unequally.
 
+----------------------------------------------
+1) Perform FL with adaptive local coefficient tau
+
+-----------------------------------------------------------
+Algorithm 1: Federated Learning with adaptive local update
+-----------------------------------------------------------
+1. The server broadcasts w0;
+2. Workers receive and initialize w0;
+3. for k= 1, ... , K do
+    The server calculates tau_k using (20);
+    The server broadcasts tau_k
+    for j=1,...,M in parallel do
+        Workers receive tau_k
+        for l=1,...,tau_k do
+            Workers compute g(wk-j,l)
+            Workers update g(wk^j,l) as in (2);
+        end
+        Workers compute g(w_k^j) as in (3);
+        Workers transmit g-hat(w_k) to the server;
+    end
+    The server receives g_hat(w_k)'s from workers;
+    The server averages g_hat(w_k)'s as in (4);
+    The server updates the global model as in (5);
+    The server broadcast w_k+1 to workers;
+   end
+-----------------------------------------------------------
+
+* Problems
+- Implementation of broadcasting process in python
+- How to measure wall clock time?
+- 
---
+++ models.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class FNN(nn.Module):
+    def __init__(self, dim_in=28*28, dim_hidden=400, dim_out=10):
+        super(FNN, self).__init__()
+        self.layer_input = nn.Linear(dim_in, dim_hidden)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(dim_hidden, dim_hidden)
+        self.fc2 = nn.Linear(dim_hidden,dim_out)
+    def forward(self, x):
+        x = x.view(-1, x.shape[1]*x.shape[-2]*x.shape[-1])
+        x = self.layer_input(x)
+        x = self.relu(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+class CNNMnist(nn.Module):
+    def __init__(self, args):
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, args.num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+
+
+class CNNCifar(nn.Module):
+    def __init__(self, args):
+        super(CNNCifar, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, args.num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.conv = nn.Sequential(
+            #3 224 128
+            nn.Conv2d(3, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #64 112 64
+            nn.Conv2d(64, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #128 56 32
+            nn.Conv2d(128, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #256 28 16
+            nn.Conv2d(256, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #512 14 8
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2)
+        )
+        #512 7 4
+
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, x):
+        features = self.conv(x)
+        x = self.avgpool(features)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
